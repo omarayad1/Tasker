b14287db5e1b003901628c6bd4a05e314f31c523
Omar H. Ayad <omarayad1@gmail.com>
2015-10-13 13:04:27 +0200

    updated gitignore

--- cpus.h
+++ cpus.h
@@ -1,12 +1,15 @@
 #ifndef CPUS_H
 #define CPUS_H
+#include <vector>
 
 class cpus {
 private:
     unsigned int count;
     unsigned int speed;
-
+    std::vector<int> cpu_data;
 public:
+    cpus();
+
 };
 
 #endif // CPU_H
